d4d5df46a40fee8bfb67bb17a09f2f6d0fafeb93
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-17 10:09:18 +0200

    bum

---
+++ Ejercicio5.java
@@ -0,0 +1,17 @@
+/*
+ * Realiza un programa que resuelva una ecuación de primer grado (del tipo ax+b =0).
+
+ */
+import java.util.Scanner;
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Este programa resuelve ecuaciones de primer grado del tipo ax + b = 0");
+        System.out.println("Por favor, introduzca el valor de a: ");
+        int a = sc.nextInt();
+        System.out.println("Ahora introduzca el valor de b: ");
+        int b = sc.nextInt();
+        System.out.println("x = "+(a*x+b=0);
+
+    }
+}
